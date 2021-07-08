488dd9fc9702f6387989ba9b3a11d065a5014877
masopego <masopego@gmail.com>
2021-07-08 18:30:42 +0200

    Add basic exercices of module 3

---
+++ module_3/Lab_3/index.js
@@ -0,0 +1,76 @@
+// Calculate Total Price of hotel booking
+const hotel = { count: 7, price: 70.55, type: "hotel" };
+
+function getTotal(product) {
+  if (product.count <= 0) {
+    return 0;
+  }
+
+  return product.count * product.price;
+}
+
+console.log(getTotal(hotel));
+
+// Calculate IVA
+
+const fruit = { count: 3, price: 0.55, type: "food" };
+const dictionary = { count: 1, price: 18.0, type: "book" };
+
+function getVat(product) {
+  let vat = 0.21;
+
+  switch (product.type) {
+    case "food":
+      vat = 0.1;
+      break;
+    case "book":
+      vat = 0.04;
+      break;
+  }
+  return (product.price * vat).toFixed(2);
+}
+
+console.log("IVA Hotel", getVat(hotel));
+console.log("IVA Fruta", getVat(fruit));
+console.log("IVA Diccionario", getVat(dictionary));
+
+// Calculate net salary
+
+const employee = {
+  raw: 8000,
+  children: 2,
+  wage: 14,
+};
+
+const rangues = {
+  first: 12000,
+  second: 24000,
+  third: 34000,
+};
+
+const reduction = 2;
+
+function calculateNetSalary(employee) {
+  let retention;
+
+  // Menos de 12.000€
+  if (employee.raw < rangues.first) {
+    retention = 0;
+  } else if (employee.raw < rangues.second) {
+    retention = 8;
+  } else if (employee.raw < rangues.third) {
+    retention = 16;
+  } else {
+    retention = 30;
+  }
+
+  if (employee.children > 0 && retention - reduction > 0) {
+    retention = retention - reduction;
+  }
+
+  console.log(retention);
+
+  return employee.raw - (employee.raw * retention) / 100;
+}
+
+console.log("Salario Neto", calculateNetSalary(employee));
