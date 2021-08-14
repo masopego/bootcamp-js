c7025c74b615c6ce791e3757740b84f5751d92f5
Marisol Pena <masopego@gmail.com>
2021-08-14 12:21:37 +0200

    Add algorithm exercise

---
+++ module_6/Lab_6.2/index.js
@@ -0,0 +1,45 @@
+const plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
+const encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
+
+const encryptedText = document.getElementById("encrypted");
+const decryptedText = document.getElementById("decrypted");
+const encryptButton = document.getElementById("encrypt_button");
+const decryptButton = document.getElementById("decrypt_button");
+
+const searchIndex = (alphabet, letter) => {
+  for (let i = 0; i < alphabet.length; i++) {
+    if (alphabet[i] === letter) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+function convertMessage(input, origin, target) {
+  let output = "";
+  for (let i = 0; i < input.length; i++) {
+    const letterIndex = searchIndex(origin, input[i]);
+
+    if (letterIndex === -1) {
+      output += input[i];
+    } else {
+      const conversionLetter = target[letterIndex];
+      output += conversionLetter;
+    }
+  }
+  return output;
+}
+
+const encrypt = (input) =>
+  convertMessage(input, plainAlphabet, encryptedAlphabet);
+
+const decrypt = (input) =>
+  convertMessage(input, encryptedAlphabet, plainAlphabet);
+
+encryptButton.addEventListener("click", () => {
+  decryptedText.value = encrypt(encryptedText.value);
+});
+
+decryptButton.addEventListener("click", () => {
+  encryptedText.value = decrypt(decryptedText.value);
+});
